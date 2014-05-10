29a05bf1412f78b1cf1454360c642d8e83d81eb7
James White <james_white_d@hotmail.com>
2014-05-10 14:03:42 +0100

    Using two fold cross validation now

--- src/task1/task1.py
+++ src/task1/task1.py
@@ -1,6 +1,7 @@
 import csv
 import numpy
 import scipy.optimize
+import random
 
 
 def loadfile(file_path):
@@ -16,6 +17,23 @@ def loadfile(file_path):
     return file_contents
 
 
+def generate_features(data):
+
+        features = []
+
+        # features += [float(item[1]) for item in data]  # Adds the 10 previous days prices as features.
+
+        features += [float(data[9][1])]  # Adds the previous days price as a feature.
+
+        # features += [float(data[9][1]) - float(data[0][1])]  # Adds the rate of change of price over prev 10 days as a feature
+
+        # features += [float(item[0]) for item in data]  # Adds the 10 previous days volume as features.
+
+        # features += [1]  # Adds a constant feature.
+
+        return features
+
+
 def compile_data(file_content):
 
     compiled_data = []
@@ -26,13 +44,7 @@ def compile_data(file_content):
 
         prev_data = file_content[i - 10: i]
 
-        features = []
-
-        features += [float(item[1]) for item in prev_data]  # Adds the 10 previous days prices as features.
-
-        # row_data += [float(item[0]) for item in prev_data]  # Adds the 10 previous days volume as features.
-
-        features += [1]  # Adds a constant feature.
+        features = generate_features(prev_data)
 
         compiled_data_row = [current_price, features]
 
@@ -41,14 +53,13 @@ def compile_data(file_content):
     return compiled_data
 
 
-def generate_training_data(data):
+def generate_data_folds(data):
 
-    return data[0::2]
+    random.shuffle(data)
 
+    folded_data = data[0::2], data[1::2]
 
-def generate_test_data(data):
-
-    return data[1::2]
+    return folded_data
 
 
 def calculate_mean_sq_error(theta, data):
@@ -68,6 +79,15 @@ def generate_theta_zero(data):
     return [0] * len(data[0][1])
 
 
+def perform_regression(theta0, training_data, test_data):
+
+    result = scipy.optimize.fmin(calculate_mean_sq_error, x0=theta0, args=tuple([training_data]), disp=False)
+
+    error = calculate_mean_sq_error(result, test_data)
+
+    return error
+
+
 def linear(file_path):
 
     file_content = loadfile(file_path)
@@ -76,12 +96,12 @@ def linear(file_path):
 
     theta0 = generate_theta_zero(data)
 
-    training_data = generate_training_data(data)
+    folded_data = generate_data_folds(data)
 
-    result = scipy.optimize.fmin(calculate_mean_sq_error, x0=theta0, args=tuple([training_data]))
+    result0 = perform_regression(theta0, folded_data[0], folded_data[1])
 
-    print "Theta = " + str(result)
+    result1 = perform_regression(theta0, folded_data[1], folded_data[0])
 
-    return calculate_mean_sq_error(result, generate_test_data(data))
+    return (result0 + result1) / 2
 
 print "MSE = " + str(linear("../../data/stock_price.csv"))
